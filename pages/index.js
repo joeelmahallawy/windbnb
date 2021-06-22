f11fb61efebb039deb2d37fc7556d7979f1b7423
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-06-22 12:49:46 -0700

    added search options and cleaned up property cards

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
+import React, { useState, useRef } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import stays from "../../stays.json";
 import logo from "../../logo.png";
 import classnames from "classnames";
-import { property } from "lodash";
-import nextConfig from "../next.config";
+import { VscStarFull } from "react-icons/vsc";
+import { IoMdSearch } from "react-icons/io";
+import { MdPlace, MdClose } from "react-icons/md";
 
 function PropertyCard({ arr }) {
   return arr.map((property, i) => {
-    console.log(property);
     return (
       <div
         className={classnames({
@@ -42,8 +43,17 @@ function PropertyCard({ arr }) {
             )}
             <div className={classnames({ [styles.type]: true })}>
               {property.type}
+
+              {`: ${property.maxGuests} guests maximum`}
             </div>
-            <span class="material-icons">&#xf0ec;</span>
+            <div className={classnames({ [styles.ratingsContainer]: true })}>
+              <VscStarFull
+                className={classnames({ [styles.ratingIcon]: true })}
+              />
+              <div className={classnames({ [styles.rate]: true })}>
+                {property.rating}
+              </div>
+            </div>
           </div>
           <div className={classnames({ [styles.propDescriptions]: true })}>
             <h4 className={classnames({ [styles.propTitle]: true })}>
@@ -57,42 +67,198 @@ function PropertyCard({ arr }) {
   });
 }
 
-// function PropertyCard({ arr, prop }) {
-//   return (
-//     <>
-//       {arr.map((property, i) => {
-//         return (
-//           <div
-//             className={classnames({
-//               [styles.propertyContainer]: true,
-//             })}
-//             key={i}
-//           >
-//             {property.city}
-//             <div
-//               className={classnames({
-//                 [styles.propPic]: true,
-//               })}
-//             ></div>
-//             <div></div>
-//             <div></div>
-//           </div>
-//         );
-//       })}
-//     </>
-//   );
+function Search() {
+  const [locationState, setLocationState] = React.useState(true);
+  const [guestState, setGuestState] = React.useState(false);
+  const [curLocation, setCurLocation] = useState("Helsinki, Finland");
+  const [adultCounter, setAdultCounter] = React.useState(0);
+  const [childrenCounter, setChildrenCounter] = React.useState(0);
+  const parentOfModal = useRef();
+  return (
+    <>
+      <div id="parentOfModal" ref={parentOfModal}>
+        <div className={classnames({ [styles.searchOptionsContainer]: true })}>
+          <button className={classnames({ [styles.closeSearch]: true })}>
+            <MdClose size="80%" color="white" />
+          </button>
+          <div className={classnames({ [styles.searchFields]: true })}>
+            <button
+              className={classnames({ [styles.locationFiller]: true })}
+              onClick={() => {
+                setLocationState(true);
+                setGuestState(false);
+              }}
+            >
+              <h4 className={classnames({ [styles.barLocation]: true })}>
+                LOCATION
+              </h4>
+              <p className={classnames({ [styles.curLocation]: true })}>
+                {curLocation}
+              </p>
+            </button>
 
-function Search({ func }) {
-  return <button></button>;
+            <button
+              className={classnames({ [styles.guestFiller]: true })}
+              onClick={() => {
+                setGuestState(true);
+                setLocationState(false);
+              }}
+            >
+              <h4 className={classnames({ [styles.barLocation]: true })}>
+                GUESTS
+              </h4>
+              <p className={classnames({ [styles.curLocation]: true })}>
+                {adultCounter + childrenCounter != 1
+                  ? `${adultCounter + childrenCounter} guests`
+                  : `${adultCounter + childrenCounter} guest`}
+              </p>
+            </button>
+            <button>
+              <div className={classnames({ [styles.searchSearch]: true })}>
+                <IoMdSearch size="50px" />
+                <h2>Search</h2>
+              </div>
+            </button>
+          </div>
+
+          <div className={classnames({ [styles.options]: true })}>
+            {locationState && (
+              <ul className={classnames({ [styles.locationOptions]: true })}>
+                <li>
+                  <MdPlace
+                    className={classnames({ [styles.locationIcons]: true })}
+                    size="7%"
+                  />
+                  <button
+                    className={classnames({
+                      [styles.locationSelections]: true,
+                    })}
+                    onClick={(e) => setCurLocation(e.target.innerText)}
+                  >
+                    Helsinki, Finland
+                  </button>
+                </li>
+                <li>
+                  <MdPlace
+                    className={classnames({ [styles.locationIcons]: true })}
+                    size="7%"
+                  />
+                  <button
+                    className={classnames({
+                      [styles.locationSelections]: true,
+                    })}
+                    onClick={(e) => setCurLocation(e.target.innerText)}
+                  >
+                    Turku, Finland
+                  </button>
+                </li>
+                <li>
+                  <MdPlace
+                    className={classnames({ [styles.locationIcons]: true })}
+                    size="7%"
+                  />
+                  <button
+                    className={classnames({
+                      [styles.locationSelections]: true,
+                    })}
+                    onClick={(e) => setCurLocation(e.target.innerText)}
+                  >
+                    Oulu, Finland
+                  </button>
+                </li>
+                <li>
+                  <MdPlace
+                    className={classnames({ [styles.locationIcons]: true })}
+                    size="7%"
+                  />
+                  <button
+                    className={classnames({
+                      [styles.locationSelections]: true,
+                    })}
+                    onClick={(e) => setCurLocation(e.target.innerText)}
+                  >
+                    Vaasa, Finland
+                  </button>
+                </li>
+              </ul>
+            )}
+            {guestState && (
+              <div className={classnames({ [styles.guestOptions]: true })}>
+                <div className={classnames({ [styles.adultContainer]: true })}>
+                  <div className={classnames({ [styles.adults]: true })}>
+                    Adults
+                  </div>
+                  <div className={classnames({ [styles.ages]: true })}>
+                    Ages 13 or above
+                  </div>
+                  <div className={classnames({ [styles.adultCounter]: true })}>
+                    <button
+                      className={classnames({ [styles.decrement]: true })}
+                      onClick={() =>
+                        setAdultCounter(adultCounter && adultCounter - 1)
+                      }
+                    >
+                      &mdash;
+                    </button>
+                    <div className={classnames({ [styles.counters]: true })}>
+                      {adultCounter}
+                    </div>
+                    <button
+                      className={classnames({ [styles.increment]: true })}
+                      onClick={() => setAdultCounter(adultCounter + 1)}
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <div className={classnames({ [styles.children]: true })}>
+                  Children
+                </div>
+                <div className={classnames({ [styles.ages]: true })}>
+                  Ages 12 and under
+                </div>
+                <div className={classnames({ [styles.childrenCounter]: true })}>
+                  <button
+                    className={classnames({ [styles.decrement]: true })}
+                    onClick={() =>
+                      setChildrenCounter(childrenCounter && childrenCounter - 1)
+                    }
+                  >
+                    &mdash;
+                  </button>
+                  <div className={classnames({ [styles.counters]: true })}>
+                    {childrenCounter}
+                  </div>
+                  <button
+                    className={classnames({ [styles.increment]: true })}
+                    onClick={() => setChildrenCounter(childrenCounter + 1)}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div
+          className={classnames({ [styles.searchOptionsBackground]: true })}
+          onClick={() => {}}
+        ></div>
+      </div>
+    </>
+  );
 }
 
 export default function Home() {
-  console.log(stays);
+  const [options, setOptions] = React.useState(false);
+
   return (
     <>
       <Head>
         <title>Windbnb</title>
       </Head>
+      {options && <Search />}
+
       <div
         className={classnames({
           [styles.navigator]: true,
@@ -100,12 +266,16 @@ export default function Home() {
       >
         <div
           className={classnames({
-            [styles.Container]: true,
+            [styles.logoContainer]: true,
           })}
         >
-          <Image className={classnames({ [styles.logo]: true })} src={logo} />
+          <Image
+            className={classnames({ [styles.logo]: true })}
+            src={logo}
+            height={30}
+            width={130}
+          />
         </div>
-
         <div
           className={classnames({
             [styles.searchBar]: true,
@@ -113,13 +283,24 @@ export default function Home() {
         >
           <button
             className={classnames({ [styles.locationBtn]: true })}
-          ></button>
-          <button className={classnames({ [styles.guestsBtn]: true })}></button>
+            onClick={() => setOptions(true)}
+          >
+            Add Location
+          </button>
+          <button className={classnames({ [styles.guestsBtn]: true })}>
+            Add Guests
+          </button>
+          <button className={classnames({ [styles.searchBtn]: true })}>
+            <IoMdSearch className={classnames({ [styles.searchIcon]: true })} />
+          </button>
         </div>
       </div>
       <div
         className={classnames({ [styles.currentSearchLocation]: true })}
       ></div>
+      <div className={classnames({ [styles.stayInFinland]: true })}>
+        <h2>Stays in Finland</h2>
+      </div>
       <div
         className={classnames({
           [styles.fullPropSectionContainer]: true,
